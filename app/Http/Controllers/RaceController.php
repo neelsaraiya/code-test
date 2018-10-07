83fcdb49ff3ee142ea9103d5b8103d64d9a45a27
Neel Saraiya <neelsaraiya@nbnco.com.au>
2018-10-07 13:25:24 +1100

    created controllers and startup test project

---
+++ app/Http/Controllers/RaceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RaceController extends Controller
+{
+    //
+    public function caulfield(){
+        echo "caulfield";
+    }
+
+    public function wolferhampton(){
+        echo "wolferhampton";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/caulfield', 'RaceController@caulfield');
+Route::get('/wolferhampton', 'RaceController@wolferhampton');
