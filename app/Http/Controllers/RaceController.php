a82610546feb5acaecbb06a29be77e09a238b62e
Neel Saraiya <neelsaraiya@nbnco.com.au>
2018-10-07 14:37:35 +1100

    implemented caulfield xml file, seperated array sort into its own function

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -16,13 +16,50 @@ use League\Flysystem\FileNotFoundException;
 class RaceController extends Controller
 {
     const WOLFERHAMPTON_FILE = 'storage/app/public/Wolferhampton_Race1.json';
+    const CAULFIELD_FILE = 'storage/app/public/Caulfield_Race1.xml';
 
 
     /**
      * This function reads Caulfield_Race1.xml and display horsenames with price in ascending order
      */
     public function caulfield(){
-        echo "caulfield";
+        //Read json file and get content into an array format to parse through it
+        try{
+            $file = base_path(self::CAULFIELD_FILE);
+            if(file_exists($file)){
+                $fileContent = file_get_contents($file);
+            }else{
+                throw new FileNotFoundException($file);
+            }
+        } catch(FileNotFoundException $e){
+            echo $e->getMessage();
+            return;
+        }
+
+        $data = simplexml_load_string($fileContent);
+
+
+        //create an array of horse names
+        $horses = [];
+        foreach($data->races->race->horses->horse as $row){
+            $horses[strval($row->number)] = [
+                'name'  =>  strval($row['name'])
+            ];
+        }
+
+        //try and match the market selections based on horsenames and add price to horses array
+        foreach($data->races->race->prices->price->horses->horse as $row){
+            foreach ($horses as $horse_id=>$horse){
+                if($horse_id == strval($row['number'])){
+                    $horses[$horse_id]['price'] = strval($row['Price']);
+                }
+            }
+        }
+
+        //Sort Multi dimensial array based on horse price
+        $horses = $this->sortByPrice($horses);
+
+        return $horses;
     }
 
     /**
@@ -66,16 +103,18 @@ class RaceController extends Controller
         }
 
         //Sort Multi dimensial array based on horse price
+        $horses = $this->sortByPrice($horses);
+
+        return $horses;
+
+    }
+
+    public function sortByPrice($horses){
         usort($horses, function($a, $b) {
             return $a['price'] - $b['price'];
         });
 
-        echo "<pre>";
-        print_r($horses);
-        //print_r($data);
-
         return $horses;
-
     }
 
 }
